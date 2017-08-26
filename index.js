d6b2632bd2969405ebe23494d18ca26d3688a44d
fraxken <gentilhomme.thomas@gmail.com>
2017-08-26 20:55:33 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const uVyper = require('uvyper');
+const redis = require('redis');
+const events = require('events');
+
+class Adapter extends events {
+
+    constructor() {
+        super();
+    }
+
+}
+
+module.exports = Adapter;
